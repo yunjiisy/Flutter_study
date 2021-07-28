3b61548915bb73f1e49d34ba0912d9d67d1d6535
yunjiisy <dbswl330@gmail.com>
2021-07-28 21:46:16 +0900

    without builder

---
+++ snackbar_2/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'snack Bar',
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Show message'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text("Hello",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white
+            ),),
+            backgroundColor: Colors.teal,
+            duration: Duration(milliseconds: 1000),
+          ),
+          );
+        },
+      ),
+    );
+  }
+}
