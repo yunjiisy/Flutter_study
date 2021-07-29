24d4ac36b76ae491ee5807587f70e8518401f61d
yunjiisy <dbswl330@gmail.com>
2021-07-29 16:48:28 +0900

    toast message

---
+++ toastmessage/android/app/src/main/kotlin/com/example/toastmessage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toastmessage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ toastmessage/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'toast message',
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title: Text('Toast Message'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            fluttertoast();
+          },
+          child: Text('Toast'),
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+void fluttertoast() {
+  Fluttertoast.showToast(
+      msg: 'Flutter',
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.redAccent,
+      fontSize: 20,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
+}
